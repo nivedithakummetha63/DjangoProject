2db2cdfc68ff85aebcfe383547d3883868cf05e1
Niveditha kummetha <224g1a0563@srit.ac.in>
2025-08-23 14:04:38 +0530

    Update urls.py

--- roomieradar/urls.py
+++ roomieradar/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +11,14 @@ urlpatterns = [
     path('chat/', include('chat.urls')),  # Chat app URLs
 ]
 
-# Serve media files during development
+# Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve media files in production (e.g. on Render)
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
