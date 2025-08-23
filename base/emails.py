b0488b37d3a2e5a77fe7b41265386e14e41fc516
Niveditha kummetha <224g1a0563@srit.ac.in>
2025-08-23 13:21:00 +0530

    Update emails.py

--- base/emails.py
+++ base/emails.py
@@ -2,17 +2,20 @@ from django.conf import settings
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
-def account_activation_email(email,email_token):
+def account_activation_email(email, email_token):
     subject = 'Activate your account'
     email_from = settings.DEFAULT_FROM_EMAIL
 
-    activation_link = f"{settings.SITE_URL}/accounts/activate/?token={token}"
+    activation_link = f"{settings.SITE_URL}/accounts/activate/?token={email_token}"
 
     html_message = render_to_string('email/activation_email.html', {'activation_link': activation_link})
-    plain_text = f"Hi , Please verify the account activation link: {activation_link}"
+    plain_text = f"Hi, Please verify your account by clicking the activation link: {activation_link}"
 
-    send_mail(subject,
-              plain_text,
-              email_from, [email],
-              html_message=html_message)
-
+    send_mail(
+        subject,
+        plain_text,
+        email_from,
+        [email],
+        html_message=html_message,
+        fail_silently=False,
+    )
